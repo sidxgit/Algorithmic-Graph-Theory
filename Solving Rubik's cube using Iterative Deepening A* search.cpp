33e61d20a6c38182f3b40063285ac5fa0392fb73
SohamSen21 <106445751+SohamSen21@users.noreply.github.com>
2023-07-15 20:07:33 +0545

    Update Solving Rubik's cube using Iterative Deepening A* search.cpp

--- Solving Rubik's cube using Iterative Deepening A* search.cpp
+++ Solving Rubik's cube using Iterative Deepening A* search.cpp
@@ -1,76 +1,141 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <unordered_map>
+#include <queue>
 #include <algorithm>
-#include <limits>
 
 using namespace std;
 
-// Function to check if the current state is the goal state
-bool isGoalState(const vector<int>& state) {
-    // Check if the state matches the goal state
-    // Return true if it is the goal state, false otherwise
-}
+// Define the Rubik's Cube class
+class RubiksCube {
+private:
+    string cubeState; // Stores the current state of the cube
+    unordered_map<char, vector<char>> moves; // Stores the possible moves for each face
 
-// Function to perform a move on the Rubik's Cube
-void performMove(vector<int>& state, int move) {
-    // Perform the move on the Rubik's Cube state
-}
-
-// Function to perform a reverse move on the Rubik's Cube
-void performReverseMove(vector<int>& state, int move) {
-    // Perform the reverse move on the Rubik's Cube state
-}
-
-// Function to calculate the heuristic value for the current state
-int calculateHeuristic(const vector<int>& state) {
-    // Calculate the heuristic value based on the distance to the goal state
-}
-
-// Function to perform the IDA* search
-bool idaStarSearch(vector<int>& state, int g, int depthLimit) {
-    int h = calculateHeuristic(state);
-    int f = g + h;
-
-    if (f > depthLimit) {
-        return false;
+public:
+    RubiksCube(string initialState) {
+        cubeState = initialState;
+        initializeMoves();
     }
 
-    if (isGoalState(state)) {
+    // Initialize the possible moves for each face
+    void initializeMoves() {
+        moves['U'] = {'F', 'R', 'B', 'L'};
+        moves['D'] = {'F', 'L', 'B', 'R'};
+        moves['F'] = {'U', 'L', 'D', 'R'};
+        moves['B'] = {'U', 'R', 'D', 'L'};
+        moves['L'] = {'U', 'F', 'D', 'B'};
+        moves['R'] = {'U', 'B', 'D', 'F'};
+    }
+
+    // Apply a move to the cube state
+    void applyMove(char face) {
+        string newState = cubeState;
+        vector<char> affectedFaces = moves[face];
+
+        for (char& affectedFace : affectedFaces) {
+            string faceState = getFaceState(affectedFace);
+            rotate(faceState.begin(), faceState.begin() + 6, faceState.end());
+            setFaceState(affectedFace, faceState);
+        }
+
+        setFaceState(face, newState);
+    }
+
+    // Get the state of a face
+    string getFaceState(char face) {
+        int startIndex = cubeState.find(face);
+        return cubeState.substr(startIndex + 1, 9);
+    }
+
+    // Set the state of a face
+    void setFaceState(char face, string newState) {
+        int startIndex = cubeState.find(face);
+        cubeState.replace(startIndex + 1, 9, newState);
+    }
+
+    // Check if the cube is solved
+    bool isSolved() {
+        for (char face = 'U'; face <= 'B'; face++) {
+            string faceState = getFaceState(face);
+            char color = faceState[0];
+            if (count(faceState.begin(), faceState.end(), color) != 9) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// IDA* search algorithm
+bool IDAStarSearch(RubiksCube& cube, string& solution, int depth, int maxDepth) {
+    if (cube.isSolved()) {
         return true;
     }
 
-    int minCost = numeric_limits<int>::max();
+    if (depth + heuristic(cube) > maxDepth) {
+        return false;
+    }
 
-    for (int move = 0; move < numMoves; ++move) {
-        if (isValidMove(state, move)) {
-            performMove(state, move);
-            if (idaStarSearch(state, g + 1, depthLimit)) {
-                return true;
-            }
-            performReverseMove(state, move);
+    for (char face = 'U'; face <= 'B'; face++) {
+        cube.applyMove(face);
+        solution.push_back(face);
+
+        if (IDAStarSearch(cube, solution, depth + 1, maxDepth)) {
+            return true;
         }
+
+        cube.applyMove(oppositeMove(face));
+        solution.pop_back();
     }
 
     return false;
 }
 
-// Function to solve the Rubik's Cube using IDA* algorithm
-void solveRubiksCubeIDAStar(vector<int>& state) {
-    int depthLimit = calculateHeuristic(state);
-
-    while (!idaStarSearch(state, 0, depthLimit)) {
-        depthLimit++;
+// Heuristic function for estimating the number of moves to solve the cube
+int heuristic(RubiksCube& cube) {
+    int count = 0;
+    for (char face = 'U'; face <= 'B'; face++) {
+        string faceState = cube.getFaceState(face);
+        char color = faceState[0];
+        count += 9 - count(faceState.begin(), faceState.end(), color);
     }
+    return count / 4;
+}
+
+// Get the opposite move of a given move
+char oppositeMove(char move) {
+    switch (move) {
+        case 'U': return 'D';
+        case 'D': return 'U';
+        case 'F': return 'B';
+        case 'B': return 'F';
+        case 'L': return 'R';
+        case 'R': return 'L';
+    }
+    return ' ';
+}
+
+// Solve the Rubik's Cube using IDA* algorithm
+string solveRubiksCube(string initialState) {
+    RubiksCube cube(initialState);
+    string solution;
+
+    for (int maxDepth = 0; maxDepth < 20; maxDepth++) {
+        if (IDAStarSearch(cube, solution, 0, maxDepth)) {
+            return solution;
+        }
+    }
+
+    return "No solution found.";
 }
 
 int main() {
-    // Initialize the Rubik's Cube state
-    vector<int> state = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    string initialState = "UWWUWWUWWUOOOYYYGGGBBBRRR";
+    string solution = solveRubiksCube(initialState);
 
-    // Solve the Rubik's Cube using IDA* algorithm
-    solveRubiksCubeIDAStar(state);
-
-    // Print the solution or perform any other desired actions
+    cout << "Solution: " << solution << endl;
 
     return 0;
 }
